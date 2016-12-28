f45f697fbf67f3c56d6f555f0132008a483f95d2
Daniel Schaal <daniel@schaal.email>
2016-12-28 17:21:21 +0100

    Add const val for preference key

--- app/src/main/kotlin/com/yubico/yubioath/YubioathApplication.kt
+++ app/src/main/kotlin/com/yubico/yubioath/YubioathApplication.kt
@@ -9,9 +9,13 @@ import android.support.v7.app.AppCompatDelegate
  */
 
 class YubioathApplication : Application() {
+    companion object {
+        const val PREF_DAY_NIGHT_MODE = "pref_day_night_mode"
+    }
+
     override fun onCreate() {
         super.onCreate()
-        val day_night_mode = PreferenceManager.getDefaultSharedPreferences(this).getInt("pref_day_night_mode", AppCompatDelegate.MODE_NIGHT_YES)
+        val day_night_mode = PreferenceManager.getDefaultSharedPreferences(this).getInt(PREF_DAY_NIGHT_MODE, AppCompatDelegate.MODE_NIGHT_YES)
         AppCompatDelegate.setDefaultNightMode(day_night_mode)
     }
 }
